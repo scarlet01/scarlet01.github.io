3a6f2eb356c251e40233c7cef75c3d48b8ecc00a
Harish Chawla <zoneix@gmail.com>
2018-12-05 00:45:35 -0500

    bug fix

--- vendor/cisco/call.js
+++ vendor/cisco/call.js
@@ -1,6 +1,6 @@
 import {getjwt} from '../cisco/guest.js';
 //var jwt = require ('../cisco/guest.js').getjwt;
 let jwt = getjwt();
-console.log jwt;
+console.log (jwt);
 document.getElementById('sometag').innerHTML = jwt;
 
